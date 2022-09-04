55733a81f675e21f01e55cd843f062a12f4d6b2a
RajPratim21 <rajpratim1234@gmail.com>
2022-09-04 14:35:50 +0530

    Updated README

---
+++ tap_columbiasports/streams.py
@@ -0,0 +1,34 @@
+"""Stream type classes for tap-columbiasports."""
+
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from singer_sdk import typing as th  # JSON Schema typing helpers
+
+from tap_columbiasports.client import ColumbiaSportsStream
+
+# TODO: Delete this is if not using json files for schema definition
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
+#       - Copy-paste as many times as needed to create multiple stream types.
+
+
+class ColumbiaSportsImageDataStream(ColumbiaSportsStream):
+    """Define custom stream."""
+
+    name = "columbia-sports-wear"
+    schema = th.PropertiesList(
+        th.Property("sizeGroup", th.StringType),
+        th.Property("styleDescription", th.StringType),
+        th.Property("colorwayName", th.StringType),
+        th.Property(
+            "publicLinks",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("link", th.StringType),
+                    th.Property("rendition", th.StringType),
+                )
+            ),
+        ),
+    ).to_dict()
+    primary_keys = ["identifier"]
